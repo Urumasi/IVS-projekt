4b27201dc730e1d7da3419700510dd0128c534b1
Urumasi <Urumasi@email.cz>
2020-03-16 19:10:26 +0100

    Add skeleton of the math lib

---
+++ src/MathLib.py
@@ -0,0 +1,117 @@
+PI = 3.141592653589793
+E = 2.718281828459045
+
+
+def add(a, b):
+	"""
+	Adds two numbers
+	:param a: First number
+	:param b: Second number
+	:return: a+b
+	"""
+	pass
+
+
+def subtract(a, b):
+	"""
+	Subtracts one number from another number
+	:param a: Number to subtract from
+	:param b: Number to subtract
+	:return: a-b
+	"""
+	pass
+
+
+def multiply(a, b):
+	"""
+	Multiplies two numbers
+	:param a: First number
+	:param b: Second number
+	:return: a*b
+	"""
+	pass
+
+
+def divide(a, b):
+	"""
+	Divides one number by another number
+	:param a: Numerator
+	:param b: Denominator
+	:return: a/b
+	"""
+	pass
+
+
+def power(base, exponent):
+	"""
+	Raises a number to a power based on the exponent
+	:param base: Number to raise to a power
+	:param exponent: Natural number (int), which power to raise the base to
+	:return: base ^ exponent
+	"""
+	pass
+
+
+def root(x, n):
+	"""
+	Takes the n-th root of a number
+	:param x: Number to take n-th root of
+	:param n: Order of the root
+	:return: n√x
+	"""
+	pass
+
+
+def sin(x):
+	"""
+	Take the sine function of x
+	:param x: Input in radians
+	:return: sin(x)
+	"""
+	pass
+
+
+def cos(x):
+	"""
+	Take the cosine function of x
+	:param x: Input in radians
+	:return: cos(x)
+	"""
+	pass
+
+
+def tan(x):
+	"""
+	Take the tangent function of x
+	:param x: Input in radians
+	:return: tan(x)
+	"""
+	pass
+
+
+def exp(x):
+	"""
+	Take the exponential function of x
+	:param x: Input number
+	:return: e^x
+	"""
+	pass
+
+
+def natural_log(x):
+	"""
+	Take the natural logarithm of x
+	:param x: Input number
+	:return: ln(x)
+	"""
+	pass
+
+
+def log(x, base):
+	"""
+	Take the logarithm of x in specific base
+	:param x: Input number
+	:param base: Base of the logarithm
+	:return: log_base(x)
+	"""
+	pass
