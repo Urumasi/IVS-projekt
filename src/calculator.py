6d768fa311b5747d5f51c5eba51bc954a8487896
precla <kneslik.m@gmail.com>
2020-04-02 14:23:44 +0200

    splitted clear() function to four functions, linebreak in long lines, added function is_result_set and constant EQUALS

--- src/calculator.py
+++ src/calculator.py
@@ -4,26 +4,33 @@ from PyQt5.QtWidgets import QApplication
 from ui_calculator import Ui_Calculator
 
 class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
-    keyPressed = QtCore.pyqtSignal(QtCore.QEvent)
+    key_pressed = QtCore.pyqtSignal(QtCore.QEvent)
+    EQUALS = "="
     def __init__(self):
         super().__init__()
         self.setupUi(self)
         self.show()
-        self.swap_style()
-        self.keyPressed.connect(self.on_key)
-
+        self.change_style()
+        self.key_pressed.connect(self.on_key)
+        
         #connect buttons
-        self.connect_buttons(self.swap_style, self.button_change)
-        self.connect_buttons(self.digit_pressed, self.button_zero, self.button_one, self.button_two, self.button_three, self.button_four, self.button_five, self.button_six, self.button_seven, self.button_eight, self.button_nine)
+        self.connect_buttons(self.change_style, self.button_change)
+        self.connect_buttons(self.digit_pressed, self.button_zero, self.button_one, 
+                            self.button_two, self.button_three, self.button_four, 
+                            self.button_five, self.button_six, self.button_seven, 
+                            self.button_eight, self.button_nine)
         self.connect_buttons(self.decimal_pressed, self.button_decimal)
         self.connect_buttons(self.result_pressed, self.button_equals)
-        self.connect_buttons(self.basic_ops_pressed, self.button_plus, self.button_minus, self.button_multiply, self.button_divide)
-        self.connect_buttons(self.clear_pressed, self.button_ce, self.button_c, self.button_del)
+        self.connect_buttons(self.basic_ops_pressed, self.button_plus, self.button_minus, 
+                            self.button_multiply, self.button_divide)
+        self.connect_buttons(self.clear_result, self.button_ce)
+        self.connect_buttons(self.clear_all, self.button_c)
+        self.connect_buttons(self.clear_last, self.button_del)
 
 
     def keyPressEvent(self, event):
         super(CalculatorWindow, self).keyPressEvent(event)
-        self.keyPressed.emit(event) 
+        self.key_pressed.emit(event) 
 
     def connect_keys(self, pressed_key, button, *keys):
         for key in keys:
@@ -42,8 +49,10 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         self.connect_keys(event.key(), self.button_eight, QtCore.Qt.Key_8)
         self.connect_keys(event.key(), self.button_nine, QtCore.Qt.Key_9)
         self.connect_keys(event.key(), self.button_decimal, QtCore.Qt.Key_Comma)
-        self.connect_keys(event.key(), self.button_equals, QtCore.Qt.Key_Enter, QtCore.Qt.Key_Equal, QtCore.Qt.Key_Return)
-        self.connect_keys(event.key(), self.button_del, QtCore.Qt.Key_Backspace, QtCore.Qt.Key_Delete)
+        self.connect_keys(event.key(), self.button_equals, QtCore.Qt.Key_Enter, 
+                        QtCore.Qt.Key_Equal, QtCore.Qt.Key_Return)
+        self.connect_keys(event.key(), self.button_del, QtCore.Qt.Key_Backspace, 
+                        QtCore.Qt.Key_Delete)
         self.connect_keys(event.key(), self.button_plus, QtCore.Qt.Key_Plus)
         self.connect_keys(event.key(), self.button_minus, QtCore.Qt.Key_Minus)
         self.connect_keys(event.key(), self.button_multiply, QtCore.Qt.Key_Asterisk)
@@ -62,59 +71,65 @@ class CalculatorWindow(QtWidgets.QMainWindow, Ui_Calculator):
         for button in buttons:
             button.show()
 
-    def swap_style(self):
+    def change_style(self):
         if self.label_style.text() == "Scientific":
-            self.hide_buttons(self.buttona, self.buttonb, self.buttonc, self.buttond, self.buttone, self.buttonf, self.buttong, self.buttonh, self.buttoni, self.buttonj, self.buttonk)
+            self.hide_buttons(self.buttona, self.buttonb, self.buttonc, self.buttond,
+                            self.buttone, self.buttonf, self.buttong, self.buttonh, 
+                            self.buttoni, self.buttonj, self.buttonk)
             self.label_style.setText("Standard")
         else:
-            self.show_buttons(self.buttona, self.buttonb, self.buttonc, self.buttond, self.buttone, self.buttonf, self.buttong, self.buttonh, self.buttoni, self.buttonj, self.buttonk)
+            self.show_buttons(self.buttona, self.buttonb, self.buttonc, self.buttond, 
+                            self.buttone, self.buttonf, self.buttong, self.buttonh, 
+                            self.buttoni, self.buttonj, self.buttonk)
             self.label_style.setText("Scientific")
-    
-    def clear(self, case):
-        if case == "CE":
-            self.line_result.setText("0")
-        elif case == "C":
-            self.line_result.setText("0")
-            self.line_subresult.setText("")
-        elif case == "CS":
-            self.line_subresult.setText("")
-        elif case == "DEL":
-            if "=" in self.line_subresult.text():
-                self.line_subresult.setText("")
-            else: 
-                self.line_result.setText(self.line_result.text()[:-1])
-                if not self.line_result.text():
-                    self.line_result.setText("0")
 
-    def clear_pressed(self):
-        button = self.sender()
-        self.clear(button.text())
+    def is_result_set(self):
+        return (self.EQUALS in self.line_subresult.text())
+
+    def clear_result(self):
+        self.line_result.setText("0")
+    
+    def clear_subresult(self):
+        self.line_subresult.setText("")
+    
+    def clear_all(self):
+        self.line_result.setText("0")
+        self.line_subresult.setText("")
+
+    def clear_last(self):
+        if self.is_result_set():
+            self.line_subresult.setText("")
+        else: 
+            self.line_result.setText(self.line_result.text()[:-1])
+            if not self.line_result.text():
+                self.line_result.setText("0")
+
     
     def digit_pressed(self):
         button = self.sender()
-        if "=" in self.line_subresult.text():
-            self.clear("C")
+        if self.is_result_set():
+            self.clear_all()
         if self.line_result.text() == "0":
             self.line_result.setText("")     
         self.line_result.setText(self.line_result.text() + button.text())
     
     def decimal_pressed(self):
         button = self.sender() 
-        if "=" in self.line_subresult.text():
-            self.clear("C")
+        if self.is_result_set():
+            self.clear_all()
         if button.text() not in self.line_result.text():
             self.line_result.setText(self.line_result.text() + button.text())
 
     def result_pressed(self):
         button = self.sender()
-        if "=" not in self.line_subresult.text():
+        if not self.is_result_set():
             self.line_subresult.setText(self.line_subresult.text() + " " + self.line_result.text() + " " + button.text())
             self.line_result.setText("result")
 
     def basic_ops_pressed(self):
         button = self.sender()
-        if "=" in self.line_subresult.text():
-            self.clear("CS")
+        if self.is_result_set():
+            self.clear_subresult()
         self.line_subresult.setText(self.line_subresult.text() + " " + self.line_result.text() + " " + button.text())
         self.line_result.setText("0")
 
