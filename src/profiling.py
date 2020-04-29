7f00860694f340409ee49666f8ba1542e4331dff
Urumasi <Urumasi@email.cz>
2020-04-30 01:28:40 +0200

    Move standard deviation calculation to separate file

--- src/profiling.py
+++ src/profiling.py
@@ -8,14 +8,9 @@ measures the speed of all function calls during the calculation.
 # Author: OkayChamps, Petr Salaba (xsalab00), FIT BUT
 # Date: 2020-Apr-29
 
-from sys import stdin, stderr
-from functools import reduce
 import cProfile
 import pstats
-
-from mathlib import MathLib
-
-__package__ = "calcchamp"
+from stddev import read_and_calculate
 
 
 # More precise time measurement for cProfile
@@ -29,29 +24,4 @@ def f8(x):
 
 pstats.f8 = f8
 
-
-def calculate_standard_deviation(values):
-	n = len(values)
-	s = MathLib.root(
-		MathLib.divide(MathLib.subtract(
-			reduce(MathLib.add, map(lambda a: MathLib.power(a, 2), values)),
-			MathLib.multiply(n, MathLib.power(MathLib.divide(reduce(MathLib.add, values), n), 2))
-		), MathLib.subtract(n, 1)), 2)
-	return s
-
-
-numbers = list()
-
-for line in stdin:
-	try:
-		line_numbers = list(map(float, line.split()))
-		numbers += line_numbers
-	except ValueError:
-		print("Attempted to parse a number with an invalid format.\n", file=stderr)
-		exit(1)
-
-if len(numbers) <= 1:
-	print("Standard deviation can't be calculated or makes no sense for 1 or 0 numbers.\n", file=stderr)
-	exit(1)
-
-cProfile.run('print(calculate_standard_deviation(numbers))')
+cProfile.run('print(read_and_calculate())')
